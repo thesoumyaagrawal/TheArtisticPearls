13b514899b2c5269ab041b09524bfc8fae6977e5
Soumya <iamsoumyaagrawal@gmail.com>
2024-04-21 12:02:44 +0530

    Product card description added

--- frontend/src/components/ProductDetails/ProductDetails.jsx
+++ frontend/src/components/ProductDetails/ProductDetails.jsx
@@ -44,7 +44,7 @@ const product = {
   details:
     'The 6-Pack includes two black, two white, and two heather gray Basic Tees. Sign up for our subscription service and be the first to get new, exciting colors, like our upcoming "Charcoal Gray" limited release.',
 };
-const reviews = { href: "#", average: 4, totalCount: 117 };
+//const reviews = { href: "#", average: 4, totalCount: 117 };
 
 const ProductDetails = () => {
   //const [selectedColor, setSelectedColor] = useState(product.colors[0]);
